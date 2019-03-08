8c5c23f4645312b52ab9f0f46a19e09f5b87e55d
Lagostaz <carol1999b@gmail.com>
2019-03-08 12:12:09 +0000

    pedir e converter

--- Cilindro/Program.cs
+++ Cilindro/Program.cs
@@ -7,18 +7,23 @@ namespace Cilindro
         static void Main(string[] args)
         {
             //declarar variáveis
-            string alturaS;
+            string alturaS, raioS;
+            float alturaF, raioF;
+            
 
             //pedir a altura
-            Console.WriteLine("Introduza as altura");
+            Console.WriteLine("Introduza a altura");
             alturaS = Console.ReadLine();
 
             //converter a altura em float 
-
+            alturaF = Convert.ToSingle(alturaS);
 
             //pedir o raio 
+            Console.WriteLine("Introduza o raio");
+            raioS = Console.ReadLine();
 
             //converter o raio em float
+            raioF = Convert.ToSingle(raioS);
 
             //calcular o volume 
 
