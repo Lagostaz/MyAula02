71795d90dbb77727d9621f2cd83958b3426d1e21
Lagostaz <carol1999b@gmail.com>
2019-03-08 12:23:19 +0000

    calcular e mostrar

--- Cilindro/Program.cs
+++ Cilindro/Program.cs
@@ -9,7 +9,8 @@ namespace Cilindro
             //declarar variáveis
             string alturaS, raioS;
             float alturaF, raioF;
-            
+            float volume, area;
+            float pi = 3.1415926f;
 
             //pedir a altura
             Console.WriteLine("Introduza a altura");
@@ -26,10 +27,15 @@ namespace Cilindro
             raioF = Convert.ToSingle(raioS);
 
             //calcular o volume 
+            volume = pi * raioF * raioF * alturaF;
 
             //carcular a area superficial 
+            area = 2 * pi * raioF * (raioF + alturaF);
 
             //apresentar os resultados 
+            Console.WriteLine($"Volume = {volume:f3}");
+            Console.WriteLine("Área Sup. = {0:f3}",area);
+
         }
     }
 }
