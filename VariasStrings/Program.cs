700bd521ff80f2973210c237e08a8435d7c68999
Lagostaz <carol1999b@gmail.com>
2019-03-08 09:53:08 +0000

    primeiro commit

--- VariasStrings/Program.cs
+++ VariasStrings/Program.cs
@@ -10,6 +10,11 @@ namespace VariasStrings
     {
         static void Main(string[] args)
         {
+            string s1 = "\t olá! \"\u00A9\"";
+            string s2 = @"\t olá! ""\u00A9""";
+
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
         }
     }
 }
